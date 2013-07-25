391705c725004b416f737105df74603a3c085b80
Max <silverhammermba@gmail.com>
2013-07-25 00:30:29 -0400

    Remove framerate option
    It just drops frames, speeding everything up

--- capture.rb
+++ capture.rb
@@ -19,4 +19,4 @@ $filename = Time.new.strftime("%F-%H%M%S") + "_#{$res}_ffmpeg.webm"
 # -vf scale= resize video
 # -an no audio
 #exec "ffmpeg -f x11grab -s #$res -i :0.0 -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -threads 4 -vf scale=-1:#$p -an #$filename"
-exec "ffmpeg -f x11grab -s #$res -r 60 -i :0.0 -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -threads 4 -an #$filename"
+exec "ffmpeg -f x11grab -s #$res -i :0.0 -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -threads 4 -an #$filename"
