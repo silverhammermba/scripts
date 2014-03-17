0518c1ed8966347ff0cc65701e107e6993f13270
Max <silverhammermba@gmail.com>
2014-03-17 19:19:42 -0400

    Fix capture script!
    Works for grabbing windows at least

--- capture.rb
+++ capture.rb
@@ -1,22 +1,26 @@
 #!/usr/bin/env ruby
 # record video of screen
-# TODO fix playback speed
 
-$res = "1600x900"
+$audio = ''
+$capture = ''
 
-$filename = Time.new.strftime("%F-%H%M%S") + "_#{$res}_ffmpeg.webm"
+if ARGV.delete('-a') or ARGV.delete('--alsa')
+  $audio = '-f alsa -i default'
+end
 
-#exec "ffmpeg -an -s #$res -r 25 -f x11grab -i :0.0 -vcodec libxvid #$filename"
+if ARGV.delete('-r') or ARGV.delete('--root')
+  $capture = '-root'
+end
 
-# -i input
-# -codec:v video encoder
-# -quality encoding speed
-# -cpu-used CPU encoding speed (lower is faster)
-# -b:v video bitrate
-# -qmin -qmax min/max quantization values
-# -maxrate -bufsize upper limit for stream bitrate
-# -threads
-# -vf scale= resize video
-# -an no audio
-#exec "ffmpeg -f x11grab -s #$res -i :0.0 -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -threads 4 -vf scale=-1:#$p -an #$filename"
-exec "ffmpeg -f x11grab -s #$res -i :0.0 -codec:v libvpx -quality good -cpu-used 0 -b:v 1000k -qmin 10 -qmax 42 -maxrate 1000k -bufsize 2000k -threads 4 -an #$filename"
+# get window dimensions
+win_info = `xwininfo #$capture`
+x = win_info[/Absolute upper-left X:\s+(\d+)/, 1].to_i
+y = win_info[/Absolute upper-left Y:\s+(\d+)/, 1].to_i
+w = win_info[/Width:\s+(\d+)/, 1].to_i
+h = win_info[/Height:\s+(\d+)/, 1].to_i
+
+$res = "#{w}x#{h}"
+
+$filename = Time.new.strftime("%F-%H%M%S") + "_#{$res}_ffmpeg.mkv"
+
+exec "ffmpeg -video_size #$res -f x11grab -i :0.0+#{x},#{y} #$audio -c:v ffvhuff -c:a flac #$filename"
