22c95cf46f7f71b2f08e0c891f4a643c77e4eb24
Max <silverhammermba@gmail.com>
2014-03-22 18:05:48 -0400

    Add id3 mode for consolidating near-miss artists

--- librarian.rb
+++ librarian.rb
@@ -3,6 +3,31 @@
 
 require 'find'
 require 'fileutils'
+require 'taglib'
+
+# create a numbered menu for a hash, with block for optional styling
+def menu hash, prompt
+  hash.each_with_index do |pair, i|
+    puts "#{i + 1}.\t#{block_given? ? yield(pair) : pair[0]}"
+  end
+
+  STDERR.print prompt
+  input = STDIN.gets
+
+  # get input until the user cancels or we get a selection
+  until input.nil? or (input.strip =~ /^(\d+)$/ and (1..hash.size) === (input = $1.to_i))
+    STDERR.puts "Invalid selection."
+    STDERR.print prompt
+    input = STDIN.gets
+  end
+
+  if input.nil?
+    STDERR.puts
+    return nil
+  end
+
+  hash.find.with_index { |pair, i| i + 1 == input }
+end
 
 if ARGV.size < 2
   STDERR.puts <<-USAGE
@@ -11,6 +36,7 @@ usage: librarian OP [DIR [DIR ...]]
 
   ext   show/delete by file extension
   dir   remove empty dirs
+  id3   music tag analysis
 USAGE
   exit 1
 end
@@ -53,6 +79,7 @@ ops = {
 
     puts
   },
+
   'dir' => proc { |library|
     # recursively remove empty directories
 
@@ -73,6 +100,54 @@ ops = {
     end
 
     STDERR.puts "#{total} empty directories removed"
+  },
+
+  'id3' => proc { |library|
+    artists = {}
+
+    normalize = proc { |n| n.gsub(/[^\w ]/, '') }
+
+    # build hash of artists grouped by normalized artist name
+    Find.find(library) do |path|
+      unless FileTest.directory?(path)
+        TagLib::FileRef.open(path) do |f|
+          if not f.null? and f.tag.artist
+            n = normalize[f.tag.artist]
+            artists[n] ||= {}
+            artists[n][f.tag.artist] ||= []
+            artists[n][f.tag.artist] << path
+          end
+        end
+      end
+    end
+
+    # get rid of unique matches
+    artists.reject! { |a, h| h.size == 1 }
+
+    # nothing to do
+    if artists.empty?
+      STDERR.puts "No near-duplicate artists"
+      next
+    end
+
+    while true
+      _, arts = menu(artists, "Examine which artist? ") { |n, a| "(#{a.size})\t#{n}" }
+      break unless arts
+      artist, _ = menu(arts, "Use which artist name? ") { |a, p| "(#{p.size})\t#{a}" }
+      break unless artist
+      STDERR.puts "Setting all artists to: #{artist}"
+      arts.each do |art, paths|
+        if art != artist
+          paths.each do |path|
+            STDERR.puts "Processing: #{path}"
+            TagLib::FileRef.open(path) do |f|
+              f.tag.artist = artist
+              f.save
+            end
+          end
+        end
+      end
+    end
   }
 }
 
