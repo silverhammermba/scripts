d4aa343b7dfee996878a1f4c4bb923756baf748b
Max <silverhammermba@gmail.com>
2014-07-07 12:11:51 -0400

    Use pulseaudio with ponymix

--- volume_osd.rb
+++ volume_osd.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby
 # display volume information using notify-send
 
-`amixer get Master` =~ /\[(\d+)%\] \[.*\] \[(on|off)\]/
+$volume = `ponymix get-volume`.to_i
+muted = system 'ponymix is-muted'
 
-$level = if $2 == "off"
-	:muted
-else
-	case $1.to_i
-	when 0...25
-		:off
-	when 25...50
-		:low
-	when 50...75
-		:medium
-	when 75..100
-		:high
-	end
+$level =
+case $volume
+when 0...25
+  :off
+when 25...50
+  :low
+when 50...75
+  :medium
+when 75..100
+  :high
 end
 
-`notify-send " " -i notification-audio-volume-#$level -h int:value:#$1 -h string:synchronous:volume`
+$level = :muted if muted
+
+`notify-send " " -i notification-audio-volume-#$level -h int:value:#$volume -h string:synchronous:volume`
