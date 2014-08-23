42482e735cef50c29dd4c9fa6dd7e4cdcf4b142a
Max <silverhammermba@gmail.com>
2014-08-23 02:33:33 -0400

    Add notification, fix Master after switch

--- headphones.rb
+++ headphones.rb
@@ -4,10 +4,16 @@
 frnt = `amixer -c PCH get Front`[/(\d+)%/, 1].to_i
 head = `amixer -c PCH get Headphone`[/(\d+)%/, 1].to_i
 
+
 if frnt > 0
   `amixer -c PCH set Front 0%`
   `amixer -c PCH set Headphone 60%`
+  source = 'headphones'
 else
-  `amixer -c PCH set Front 100%`
   `amixer -c PCH set Headphone 0%`
+  `amixer -c PCH set Front 100%`
+  source = 'speakers'
 end
+`amixer -c PCH set Master 100%`
+
+`notify-send 'Enabling #{source}' --icon=multimedia-volume-control`
