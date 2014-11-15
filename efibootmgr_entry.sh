b770e9abbc0e5f81fd4f42cac92792922ee1b371
Max <silverhammermba@gmail.com>
2014-11-15 13:36:14 -0500

    Better script for updating boot options

--- efibootmgr_entry.sh
+++ efibootmgr_entry.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-efibootmgr -c -d /dev/sda -p 1 -l /vmlinuz-linux -L "Arch Linux" -u 'root=UUID=a487a5aa-c376-4a43-bd8c-35692a8fb46b rw initrd=/initramfs-linux.img acpi_osi="!Windows 2012"'
+# create new Arch Linux UEFI boot entry
+
+LABEL="Arch Linux"
+ENTRY="$(efibootmgr | grep "$LABEL" | cut --characters=5-8)"
+
+if [ -n "$ENTRY" ]; then
+	echo >&2 "Deleting old entry..."
+
+	efibootmgr \
+		--bootnum "$ENTRY" \
+		--delete-bootnum \
+		> /dev/null
+fi
+
+echo >&2 "Creating new entry..."
+
+efibootmgr \
+	--create \
+	--disk /dev/sda \
+	--part 1 \
+	--loader /vmlinuz-linux \
+	--label "$LABEL" \
+	--unicode 'root=UUID=a487a5aa-c376-4a43-bd8c-35692a8fb46b rw initrd=/intel-ucode.img initrd=/initramfs-linux.img acpi_osi="!Windows 2012"' \
+	> /dev/null
+
+efibootmgr -v | grep "$LABEL"
