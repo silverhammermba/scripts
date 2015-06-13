96fec0b98662a05ab20eb03db35d565527fb3542
Max <silverhammermba@gmail.com>
2015-06-12 23:46:22 -0400

    Add argument for editing multiple files at once

--- vtag.rb
+++ vtag.rb
@@ -19,27 +19,70 @@ def sec2hms sec
   end
 end
 
-# load file with name, return string with tag information
-def tag_str_from_file name
-  readonly = nil
-  writeable = nil
+$write_keys = %w{album artist comment genre title track year}
 
+# load file name and return hash of readonly properties
+def readonly_from_file name
   TagLib::FileRef.open(name) do |f|
     if f.null?
       raise Errno::ENOENT, "No such file or directory - #{name}"
     end
 
     props = f.audio_properties
-    readonly = {
+    return {
       'bitrate' => "#{props.bitrate} kb/s",
       'channels' => props.channels,
       'length' => "#{sec2hms props.length}",
       'sample rate' => "#{props.sample_rate} Hz"
     }
-
-    write_keys = %w{album artist comment genre title track year}
-    writeable = write_keys.map { |key| [key, f.tag.send(key)] }.to_h
   end
+end
+
+# load file with name, return hash with tag information
+def kv_from_file name
+  TagLib::FileRef.open(name) do |f|
+    if f.null?
+      raise Errno::ENOENT, "No such file or directory - #{name}"
+    end
+
+    return $write_keys.map { |key| [key, f.tag.send(key)] }.to_h
+  end
+end
+
+# edit tag fields in file name using hash
+def kv_to_file hash, name
+  TagLib::FileRef.open(name) do |f|
+    if f.null?
+      raise Errno::ENOENT, "No such file or directory - #{name}"
+    end
+
+    return if hash.empty?
+
+    hash.each do |key, value|
+      # don't write empty strings, just clear the field
+      value = nil if value.is_a?(String) && value.empty?
+
+      begin
+        f.tag.send("#{key}=", value)
+      rescue TypeError
+        # try converting numeral strings to integers
+        if value.is_a?(String) && value =~ /^\d+$/
+          value = value.to_i
+          retry
+        end
+
+        raise
+      end
+    end
+
+    f.save
+  end
+end
+
+# load file with name, return string with tag information
+def tag_str_from_file name
+  readonly = readonly_from_file name
+  writeable = kv_from_file name
 
   str = StringIO.new
 
@@ -59,31 +102,59 @@ def tag_str_from_file name
   str.string
 end
 
+# convert YAML to a Hash (very naively)
+def kv_from_str str
+  str.lines.select { |line| line =~ /^\s*[^#].*:/ }.map { |line| line.split(?:, 2).map(&:strip) }
+end
+
 # parse tag information from str, save to tags in file name
 def tag_str_to_file str, name
-  kv = str.lines.select { |line| line =~ /^\s*[^#].*:/ }.map { |line| line.split(?:, 2).map(&:strip) }
+  kv = kv_from_str str
 
-  TagLib::FileRef.open(name) do |f|
-    if f.null?
-      raise Errno::ENOENT, "No such file or directory - #{name}"
+  kv_to_file kv, name
+end
+
+if ARGV.delete('--common')
+  hashes = ARGV.map { |name| kv_from_file(name).to_h }
+
+  # find which tag fields are common among the files
+  keys = hashes.flat_map { |hash| hash.keys }.uniq
+  common = keys.select { |key| hashes.map { |hash| hash[key] }.uniq.compact.size <= 1 }
+  leftover = $write_keys - common
+
+  kv = nil
+
+  # output common keys for editing
+  Tempfile.open([$0, '.yaml']) do |temp|
+    key_length = $write_keys.map(&:length).max
+
+    common.each do |key|
+      value = hashes.map { |hash| hash[key] }.uniq.compact[0]
+      temp.puts "  %#{key_length}s: #{value}" % key
+    end
+    leftover.each do |key|
+      temp.puts "# %#{key_length}s:" % key
+    end
+    temp.puts
+
+    ARGV.each do |name|
+      temp.puts "# #{name}"
     end
 
-    kv.each do |key, value|
-      value = nil if value.empty?
-      begin
-        f.tag.send("#{key}=", value)
-      rescue TypeError
-        if value.is_a?(String) && value =~ /^\d+$/
-          value = value.to_i
-          retry
-        else
-          raise
-        end
-      end
-    end
+    temp.flush
 
-    f.save
+    system(ENV['EDITOR'] || "vim", temp.path)
+
+    temp.rewind
+    kv = kv_from_str temp.read
   end
+
+  # save changes
+  ARGV.each do |name|
+    kv_to_file kv, name
+  end
+
+  exit
 end
 
 ARGV.each do |name|
