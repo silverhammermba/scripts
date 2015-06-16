24ea42858d27f3614b6cb391b77b49cbb223378c
Max <silverhammermba@gmail.com>
2015-06-16 00:40:16 -0400

    refactor tag r/w into class

--- vtag.rb
+++ vtag.rb
@@ -7,6 +7,84 @@ require 'taglib'
 require 'tempfile'
 require 'yaml'
 
+class Integer
+  # convert seconds to H:M:S
+  def to_hms
+    h = self / 3600
+    m = (self - h * 3600) / 60
+    s = self - h * 3600 - m * 60
+    if h > 0
+      "%d:%02d:%02d" % [h, m, s]
+    elsif m > 0
+      "%d:%02d" % [m, s]
+    else
+      "#{s} s"
+    end
+  end
+end
+
+# wrapper around TagLib::FileRef
+class AudioFile
+  WRITE_KEYS = %w{album artist comment genre title track year}
+
+  attr_reader :name
+
+  def initialize name
+    @name = name
+  end
+
+  # get readonly properties
+  def readonly_props
+    open do |f|
+      props = f.audio_properties
+      {
+        'bitrate' => "#{props.bitrate} kb/s",
+        'channels' => props.channels,
+        'length' => "#{props.length.to_hms}",
+        'sample rate' => "#{props.sample_rate} Hz"
+      }
+    end
+  end
+
+  # get editable tag fields
+  def props
+    open do |f|
+      WRITE_KEYS.map { |key| [key, f.tag.send(key)] }.to_h
+    end
+  end
+
+  # set tag fields
+  def props= tags
+    return if tags.empty?
+
+    open do |f|
+      tags.each do |key, value|
+        # don't write empty strings, just clear the field
+        value = nil if value.is_a?(String) && value.empty?
+
+        begin
+          f.tag.send("#{key}=", value)
+        rescue TypeError
+          # try converting to int if edit failed
+          value = Integer(value, 10)
+          retry
+        end
+      end
+
+      f.save
+    end
+  end
+
+  private
+
+  def open
+    TagLib::FileRef.open(@name) do |f|
+      raise Errno::ENOENT, "No such audio file - #@name" if f.null?
+      return yield(f)
+    end
+  end
+end
+
 # open temp YAML file(s) for editing, return edited contents
 def edit_tmp_yaml num = nil
   temps = Array.new(num || 1) { Tempfile.new([$0, '.yaml']) }
@@ -18,9 +96,7 @@ def edit_tmp_yaml num = nil
   end
 
   temps.each(&:flush)
-
   system(ENV['EDITOR'] || 'vim', *temps.map(&:path))
-
   temps.each(&:rewind)
 
   if num
@@ -32,110 +108,19 @@ ensure
   temps.each(&:close)
 end
 
-# convert seconds to H:M:S
-def sec2hms sec
-  h = sec / 3600
-  m = (sec - h * 3600) / 60
-  s = sec - h * 3600 - m * 60
-  if h > 0
-    "%d:%02d:%02d" % [h, m, s]
-  elsif m > 0
-    "%d:%02d" % [m, s]
-  else
-    "#{s} s"
-  end
-end
-
-$write_keys = %w{album artist comment genre title track year}
-
-# load file name and return hash of readonly properties
-def readonly_hash_from_file name
-  TagLib::FileRef.open(name) do |f|
-    if f.null?
-      raise Errno::ENOENT, "No such file or directory - #{name}"
-    end
-
-    props = f.audio_properties
-    return {
-      'bitrate' => "#{props.bitrate} kb/s",
-      'channels' => props.channels,
-      'length' => "#{sec2hms props.length}",
-      'sample rate' => "#{props.sample_rate} Hz"
-    }
-  end
-end
-
-# load file name, return hash with tag information
-def tag_hash_from_file name
-  TagLib::FileRef.open(name) do |f|
-    if f.null?
-      raise Errno::ENOENT, "No such file or directory - #{name}"
-    end
-
-    return $write_keys.map { |key| [key, f.tag.send(key)] }.to_h
-  end
-end
-
-# edit tag fields in file name using hash
-def tag_hash_to_file tags, name
-  TagLib::FileRef.open(name) do |f|
-    if f.null?
-      raise Errno::ENOENT, "No such file or directory - #{name}"
-    end
-
-    return if tags.empty?
-
-    tags.each do |key, value|
-      # don't write empty strings, just clear the field
-      value = nil if value.is_a?(String) && value.empty?
-
-      begin
-        f.tag.send("#{key}=", value)
-      rescue TypeError
-        value = Integer(value, 10)
-        retry
-      end
-    end
-
-    f.save
-  end
-end
-
-# load file with name, return string with tag information
-def tag_str_from_file name
-  readonly = readonly_hash_from_file name
-  writeable = tag_hash_from_file name
-
-  str = StringIO.new
-
-  str.puts "# #{name}"
-  str.puts "#"
-  readonly.each { |k, v| str.puts "# #{k}: #{v}" }
-  str.puts YAML.dump(writeable)
-
-  str.string
-end
-
-# parse tag hash from string
-def tag_hash_from_str str
-  YAML.load(str)
-end
-
-# parse tag hash from str, save to tags in file name
-def tag_str_to_file str, name
-  tag_hash_to_file tag_hash_from_str(str), name
-end
+### MAIN PROCEDURES ###
 
 # edit one tag file for all args
 def edit_all names
-  hashes = names.map { |name| tag_hash_from_file(name).to_h }
+  files = names.map { |name| AudioFile.new(name) }
+  hashes = files.map(&:props)
 
   # find which tag fields are common among the files
   keys = hashes.flat_map { |hash| hash.keys }.uniq
   common = keys.select { |key| hashes.map { |hash| hash[key] }.uniq.compact.size <= 1 }
-  leftover = $write_keys - common
+  leftover = AudioFile::WRITE_KEYS - common
 
-  str = edit_tmp_yaml do |temp|
+  new_str = edit_tmp_yaml do |temp|
     # print common tags
     ck = common.map do |key|
       [key, hashes.map { |hash| hash[key] }.uniq.compact[0]]
@@ -154,28 +139,43 @@ def edit_all names
   end
 
   # save changes
-  names.each do |name|
-    tag_hash_to_file tag_hash_from_str(str), name
+  props = YAML.load(new_str)
+  files.each do |file|
+    file.props = props
   end
 end
 
 # edit tag file for each arg
 def edit_multiple names
-  strs = names.map { |name| tag_str_from_file name }
+  files = names.map { |name| AudioFile.new(name) }
+
+  strs = files.map do |file|
+    str = StringIO.new
+
+    str.puts "# #{file.name}"
+    str.puts "#"
+    file.readonly_props.each { |k, v| str.puts "# #{k}: #{v}" }
+    str.puts YAML.dump(file.props)
+
+    str.string
+  end
+
   new_strs = edit_tmp_yaml(names.size) do |tmps|
     strs.zip(tmps).each { |str, tmp| tmp.write str }
   end
 
-  names.zip(strs, new_strs).each do |name, str, new_str|
+  files.zip(strs, new_strs).each do |file, str, new_str|
     # TODO more robust check for changes?
     if str == new_str
       next
     end
-    tag_str_to_file new_str, name
+    file.props = YAML.load(new_str)
   end
 end
 
+# default behavior
 op = :edit_multiple
+
 OptionParser.new do |opts|
   opts.banner = "Usage: #$0 [--all] [FILES]"
 
