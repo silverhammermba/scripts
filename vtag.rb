d77e8940e12be3d4d99ee5f5864029e7d55d844d
Max <silverhammermba@gmail.com>
2015-08-03 10:42:01 -0400

    Fix tempfile name containing slashes

--- vtag.rb
+++ vtag.rb
@@ -89,7 +89,8 @@ end
 
 # open temp YAML file(s) for editing, return edited contents
 def edit_tmp_yaml num = nil
-  temps = Array.new(num || 1) { Tempfile.new([$0, '.yaml']) }
+  name = File.basename($0, File.extname($0))
+  temps = Array.new(num || 1) { Tempfile.new([name, '.yaml']) }
 
   if num
     yield temps
